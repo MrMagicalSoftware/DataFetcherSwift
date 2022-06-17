fb42853b8549ebcb86586367e9f105c22594409d
mr <cds>
2022-06-17 10:32:54 +0200

    Initial Commit

---
+++ DataFetcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataFetcher
+//
+//  Created by mr on 17/06/22.
+//  Copyright © 2022 mr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
